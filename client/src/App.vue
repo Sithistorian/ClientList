f59852fa405697e12506fbfc956a394f1ff78e12
Tristan <tristanshawnjohnson@gmail.com>
2021-09-25 14:13:03 -0500

    Refactored getAll to populate data with clients array and providers array

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,133 +4,134 @@
 
 
 <script>
-import requests from './logic/requests.js';
 import axios from 'axios';
 
 export default {
   data() {
     return {
       message: 'Vue is working just fine!',
-      information: null
+      clients: null,
+      providers: null
     }
   },
   methods: {
-  getAll: function(populated = 'false') {
+    getAll: function(populated = 'false') {
+
+      var config = {
+        method: 'get',
+        url: `http://localhost:3000/all/?populated=${populated}`
+      };
+
+      axios(config)
+      .then(res => {
+        console.log(res.data)
+        this.clients = res.data.clients;
+        this.providers = res.data.providers;
+        return res.data;
+      })
+      .catch(err =>  {
+        console.log('Something went wrong', err)
+      });
+    },
+    createClient: function(client) {
+    var data = JSON.stringify(client);
 
     var config = {
-      method: 'get',
-      url: `http://localhost:3000/all/?populated=${populated}`
+      method: 'post',
+      url: 'http://localhost:3000/clients/createClient/',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data : data
     };
 
     axios(config)
     .then(res => {
-      console.log(res.data)
-      this.information = res.data
-      return res.data;
+      console.log(res.data);
+      this.getAll();
     })
-    .catch(err =>  {
-      console.log('Something went wrong', err)
+    .catch(err => console.log('Something went wrong', err));
+    },
+    modifyClient: function(obj) {
+    var data = JSON.stringify(obj);
+
+    var config = {
+      method: 'put',
+      url: 'http://localhost:3000/clients/modifyClient',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+
+    axios(config)
+    .then(res => {
+      console.log(res.data);
+      this.getAll()
+      })
+    .catch(err => console.log('Something went wrong', err));
+    },
+    deleteClient: function(clientId) {
+
+    var data = JSON.stringify({
+      "clientId": clientId
     });
-  },
-  createClient: function(client) {
-  var data = JSON.stringify(client);
 
-  var config = {
-    method: 'post',
-    url: 'http://localhost:3000/clients/createClient/',
-    headers: {
-      'Content-Type': 'application/json'
+    var config = {
+      method: 'post',
+      url: 'http://localhost:3000/clients/deleteClient/',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+
+    axios(config)
+    .then(res => {
+      console.log(res.data);
+      this.getAll();
+      })
+    .catch(err => console.log('Something went wrong', err));
+
     },
-    data : data
-  };
+    createNewProvider: function(provider) {
+    var data = JSON.stringify(provider);
 
-  axios(config)
-  .then(res => {
-    console.log(res.data);
-    this.getAll();
-  })
-  .catch(err => console.log('Something went wrong', err));
-  },
-  modifyClient: function(obj) {
-  var data = JSON.stringify(obj);
+    var config = {
+      method: 'post',
+      url: 'http://localhost:3000/providers/createNewProvider/',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
 
-  var config = {
-    method: 'put',
-    url: 'http://localhost:3000/clients/modifyClient',
-    headers: {
-      'Content-Type': 'application/json'
+    axios(config)
+    .then(res => {
+      console.log(res.data);
+      this.getAll();
+      })
+    .catch(err => console.log('Something went wrong', err));
     },
-    data : data
-  };
+    changeProviderName: function(obj) {
+    var data = JSON.stringify(obj);
 
-  axios(config)
-  .then(res => {
-    console.log(res.data);
-    this.getAll()
+    var config = {
+      method: 'put',
+      url: 'http://localhost:3000/providers/changeName',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+
+    axios(config)
+    .then(res => {
+      console.log(res.data);
+      this.getAll()
     })
-  .catch(err => console.log('Something went wrong', err));
-  },
-  deleteClient: function(clientId) {
-
-  var data = JSON.stringify({
-    "clientId": clientId
-  });
-
-  var config = {
-    method: 'post',
-    url: 'http://localhost:3000/clients/deleteClient/',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    data : data
-  };
-
-  axios(config)
-  .then(res => {
-    console.log(res.data);
-    this.getAll();
-    })
-  .catch(err => console.log('Something went wrong', err));
-
-},
-  createNewProvider: function(provider) {
-  var data = JSON.stringify(provider);
-
-  var config = {
-    method: 'post',
-    url: 'http://localhost:3000/providers/createNewProvider/',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    data : data
-  };
-
-  axios(config)
-  .then(res => {
-    console.log(res.data);
-    this.getAll();
-    })
-  .catch(err => console.log('Something went wrong', err));
-},
-  changeProviderName: function(obj) {
-  var data = JSON.stringify(obj);
-
-  var config = {
-    method: 'put',
-    url: 'http://localhost:3000/providers/changeName',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    data : data
-  };
-
-  axios(config)
-  .then(res => {
-    console.log(res.data);
-    this.getAll()
-  })
-  .catch(err => console.log('Something went wrong', err));
-}
+    .catch(err => console.log('Something went wrong', err));
+    }
   },
   mounted() {
     this.getAll()
