b39fc5f15fa81c6a9c87cbbad451ac9d693a1c38
Tristan <tristanshawnjohnson@gmail.com>
2021-09-26 20:22:46 -0500

    Formating the form a little bit

--- client/src/components/Form.vue
+++ client/src/components/Form.vue
@@ -1,11 +1,26 @@
 <template>
-<h2>This is the form</h2>
+<h1>The form is rendering</h1>
+  <form>
+  <label for="clientName">Name</label>
+  <input  id="clientName" type="text"/><br>
+  <label for="clientEmail">Email</label>
+  <input  id="clientEmail" type="email"/><br>
+  <label for="clientPhone">Phone</label>
+  <input  id="clientPhone" type="text"/><br>
+  <label for="clientProviders">Providers</label>
+  <input  id="clientProvidrs" type="text"/>
+  <button>Add Provider</button>
+    <ProvidersDropDown :provider="provider"></ProvidersDropDown>
+  </form>
+
 
 </template>
 
 <script>
+import ProvidersDropDown from './ProvidersDropDown.vue'
 
 export default {
+  components: { ProvidersDropDown },
 
   props: [
     "clientName",
---
+++ client/src/components/ProvidersDropDown.vue
@@ -0,0 +1,19 @@
+<template>
+
+<option>{{this.provider.name}}</option>
+
+</template>
+
+
+<script>
+
+export default {
+
+  props: ["provider"]
+
+}
+</script>
+
+<style scoped>
+
+</style>
