6d33c905074f29d72827794ef9b72ce635d551e5
Tristan <tristanshawnjohnson@gmail.com>
2021-09-27 13:42:30 -0500

    Added the edit and trash icons

--- client/src/components/ProvidersCheckBoxes.vue
+++ client/src/components/ProvidersCheckBoxes.vue
@@ -2,6 +2,8 @@
 
 <input type="checkbox" id="this.provider._id"/>
 <label for="this.provider._id">{{this.provider.name}}</label>
+<button><i class="fa fa-edit"></i></button>
+<button><i class="fa fa-trash"></i></button>
 
 </template>
 
