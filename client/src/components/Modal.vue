1045c92bc8c58f168e44f95a9cc91bd8c6b1c006
Tristan <tristanshawnjohnson@gmail.com>
2021-09-28 11:48:25 -0500

    Had the labels wrong

--- client/src/components/Modal.vue
+++ client/src/components/Modal.vue
@@ -3,8 +3,8 @@
 <h1 v-if="showNewClientModal">Adding a Client</h1>
 <h1>Client</h1>
 <Form :clientName="clientName" :clientEmail="clientEmail" :clientPhone="clientPhone" :clientProviders="clientProviders" :providers="providers" @get-all="this.$emit('get-all')"></Form>
-<h1 v-if="showEditModal"><button>Delete</button><button>Cancel</button><button>Add Client</button></h1>
-<h1 v-if="showNewClientModal"><button>Cancel</button><button>Save Client</button></h1>
+<h1 v-if="showEditModal"><button>Delete</button><button>Cancel</button><button>Save Client</button></h1>
+<h1 v-if="showNewClientModal"><button>Cancel</button><button>Add Client</button></h1>
 </template>
 
 <script>
