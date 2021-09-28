71163eb8fb06c46b9e40c13b7991000b84452dfd
Tristan <tristanshawnjohnson@gmail.com>
2021-09-28 13:41:43 -0500

    Implemented the functionality for the edit cancel button

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <modal v-if="showEditModal | showNewClientModal" :clientName="clientName" :clientEmail="clientEmail" :clientPhone="clientPhone" :clientProviders="clientProviders" :showEditModal="showEditModal" :showNewClientModal="showNewClientModal" :providers="providers" @get-all="getAll"></modal>
+  <modal v-if="showEditModal | showNewClientModal" :clientName="clientName" :clientEmail="clientEmail" :clientPhone="clientPhone" :clientProviders="clientProviders" :showEditModal="showEditModal" :showNewClientModal="showNewClientModal" :providers="providers" @get-all="getAll" @toggle-edit-modal="toggleShowEditModal"></modal>
   <h1>Clients<span>
     <button @click="showNewClientModal = true">New Client</button>
     </span></h1>
--- client/src/components/Modal.vue
+++ client/src/components/Modal.vue
@@ -1,10 +1,20 @@
 <template>
 <h1 v-if="showEditModal">Editing a Client</h1>
 <h1 v-if="showNewClientModal">Adding a Client</h1>
+
 <h1>Client</h1>
+
 <Form :clientName="clientName" :clientEmail="clientEmail" :clientPhone="clientPhone" :clientProviders="clientProviders" :providers="providers" @get-all="this.$emit('get-all')"></Form>
-<h1 v-if="showEditModal"><button form="form">Delete</button><button form="form">Cancel</button><button form="form">Save Client</button></h1>
-<h1 v-if="showNewClientModal"><button form="form">Cancel</button><button form="form">Add Client</button></h1>
+
+<h1 v-if="showEditModal">
+  <button form="form" type="submit">Delete</button>
+  <button form="form" type="submit" @toggle-edit-modal="this.emit('toggle-edit-modal')">Cancel</button>
+  <button form="form" type="submit">Save Client</button>
+</h1>
+<h1 v-if="showNewClientModal">
+  <button form="form" type="submit">Cancel</button>
+  <button form="form" type="submit">Add Client</button>
+</h1>
 </template>
 
 <script>
